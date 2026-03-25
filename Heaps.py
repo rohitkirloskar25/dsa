e65ae5e4d1b69f5ac1ada58995095df8e39f9bbc
Rohit Kirloskar <rohit.kirloskar@tvh.com>
2026-03-25 18:09:15 +0530

    Added Heaps

---
+++ Heaps.py
@@ -0,0 +1,65 @@
+class MaxHeap:
+    def __init__(self):
+        self.heap = []
+
+    def _left_child(self, index):
+        return (2 * index) + 1
+    
+    def _right_child(self, index):
+        return (2 * index) + 2
+    
+    def _parent(self, index):
+        return (index-1) // 2
+    
+    def _swap(self, index1, index2):
+        self.heap[index1], self.heap[index2] = self.heap[index2], self.heap[index1]
+
+    def insert(self, value):
+        self.heap.append(value)
+        current = len(self.heap) - 1
+
+        while current > 0 and self.heap[current] > self.heap[self._parent(current)]:
+            self._swap(current, self._parent(current))
+            current = self._parent(current)
+
+    def remove(self, value):
+        if len(self.heap) == 0: return None
+        if len(self.heap) == 1: self.heap.pop()
+
+        max_value = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.sync_down(0)
+
+        return max_value
+
+    def sync_down(self, index):
+        max_index = index
+        
+        while True:
+            left_index = self._left_child(index)
+            right_index = self._right_child(index)  
+
+            if left_index < len(self.heap) and self.heap[left_index] > self.heap[max_index]: 
+                max_index = left_index
+
+            if right_index < len(self.heap) and self.heap[right_index] > self.heap[max_index]:
+                max_index = right_index
+
+            if max_index != index:
+                self._swap(max_index, index)
+                index = max_index
+            
+            else:
+                return
+
+heap = MaxHeap()
+heap.insert(1)
+heap.insert(2)
+heap.insert(3)
+heap.insert(10)
+heap.insert(1)
+heap.insert(2)
+heap.insert(3)
+print(heap.heap)
+heap.remove(10)
+print(heap.heap)
